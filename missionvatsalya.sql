0f69759310103ab07ca414ec143f36725b26b433
Kiran Sankar Das <kiran.sankar.das@gmail.com>
2024-07-20 19:49:57 +0530

    Minor data entries

--- missionvatsalya.sql
+++ missionvatsalya.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 19, 2024 at 06:57 PM
+-- Generation Time: Jul 20, 2024 at 07:49 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
